f54d6605c0c9afa46760b3ebde8f1903d8a3da44
Shrikanth Rajgopalan <shrikanth.rajgopalan@gmail.com>
2018-07-27 20:25:12 +0800

    Initial Commit of project structure
    Signed-off-by: Shrikanth Rajgopalan <shrikanth.rajgopalan@gmail.com>

---
+++ src/main/java/com/srajgopalan/camel/springboot/file2MailAndDb/File2MailAndDbApplication.java
@@ -0,0 +1,12 @@
+package com.srajgopalan.camel.springboot.file2MailAndDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class File2MailAndDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(File2MailAndDbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/srajgopalan/camel/springboot/file2MailAndDb/File2MailAndDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.srajgopalan.camel.springboot.file2MailAndDb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class File2MailAndDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
