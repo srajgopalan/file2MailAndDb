fd330555b39f248e1cb7f08a19535e6e97c020d4
Shrikanth Rajgopalan <shrikanth.rajgopalan@gmail.com>
2018-07-30 13:12:52 +0800

    added a simple junit test case for simple file transfer
    Signed-off-by: Shrikanth Rajgopalan <shrikanth.rajgopalan@gmail.com>

---
+++ src/test/java/com/srajgopalan/camel/springboot/file2MailAndDb/route/SimpleRouteTestBasic.java
@@ -0,0 +1,54 @@
+package com.srajgopalan.camel.springboot.file2MailAndDb.route;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.File;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+
+public class SimpleRouteTestBasic {
+
+    @Autowired
+    ProducerTemplate producerTemplate;
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void simpleRouteTest() throws InterruptedException {
+        String message = "this is a test message";
+        String filename = "test.txt";
+
+        //inject the file
+        producerTemplate.sendBodyAndHeader(environment.getProperty("fromRoute"), message,
+                Exchange.FILE_NAME, filename);
+
+        Thread.sleep(5000);
+
+        //now check if the output file exists
+
+        File outputDir = new File("/tmp/camel/output");
+
+        File outputFile = new File( "/tmp/camel/output/" + filename);
+
+        assertTrue(outputDir.exists());
+        assertNotEquals(0, outputDir.listFiles());
+        assertTrue(outputFile.exists());
+
+    }
+}
